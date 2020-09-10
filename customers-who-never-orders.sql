e3f863827daf6203a2a9f910ba362e6e13056489
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-09-10 16:50:54 -0500

    Update customers-who-never-orders.sql

--- customers-who-never-orders.sql
+++ customers-who-never-orders.sql
@@ -3,4 +3,9 @@ SELECT  name as Customers
 from customers c
 left join orders o
 on c.id = o.CustomerId 
-where o.CustomerId is Null
+where o.CustomerId is Null;
+
+
+# Faster solution
+select Name as Customers from Customers where Id not in (select CustomerId from Orders);
+
