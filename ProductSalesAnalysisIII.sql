6e02c8013621b84a33c5ed0a5bc64acdd3245a5c
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-07 23:39:03 -0500

    Update ProductSalesAnalysisIII.sql

--- ProductSalesAnalysisIII.sql
+++ ProductSalesAnalysisIII.sql
@@ -24,3 +24,13 @@ select s.product_id, s.year as first_year, s.quantity,s.price
 from sales s
 inner join firstyearCTE f
 on s.product_id = f.product_id and s.year = f.firstyear;
+
+
+-- Alternative approach
+
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+SELECT product_id, MIN(year) as year
+FROM Sales
+GROUP BY product_id) ;
