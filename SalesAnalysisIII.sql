0038602827a22dd3e27798ec293bc8b9fb8b263e
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-08 23:03:41 -0500

    Update SalesAnalysisIII.sql

--- SalesAnalysisIII.sql
+++ SalesAnalysisIII.sql
@@ -11,3 +11,13 @@ select product_id,product_name
 from cte 
 group by product_id,product_name
 having min(sale_date) >= "2019-01-01" and max(sale_date) <= "2019-03-31";
+
+
+
+-- Better Solution
+SELECT product_id, product_name
+FROM Sales 
+JOIN Product 
+Using(product_id)
+GROUP BY product_id
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31' 
