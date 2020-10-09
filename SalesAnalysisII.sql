6ef6b4e5c6339006fb2d80517a11cb942dea1000
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-08 22:49:57 -0500

    Create SalesAnalysisII.sql

---
+++ SalesAnalysisII.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+
+with cte as (
+select s.buyer_id,p.product_name
+from sales s
+left join product p
+on s.product_id = p.product_id
+)
+
+
+select buyer_id
+from cte 
+where product_name = "S8" and buyer_id not in (select buyer_id from cte where product_name = 'iPhone');
