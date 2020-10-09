73b211b7bb507424599c2eeb76ad977e37b1cc49
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-08 23:49:03 -0500

    Update useractivitypast30daysI.sql

--- useractivitypast30daysI.sql
+++ useractivitypast30daysI.sql
@@ -17,3 +17,12 @@ where totalactivity >= 1 and activity_date >= "2019-06-28"  and activity_date <=
 select activity_date as day, count(user_id) as active_users
 from activeusersCTE
 group by activity_date;
+
+
+-- better solution
+-- since there is an entry in the table, all users are bydefault active
+
+select activity_date as day, count(distinct user_id) as active_users 
+from Activity
+where datediff('2019-07-27', activity_date) <30
+group by activity_date
