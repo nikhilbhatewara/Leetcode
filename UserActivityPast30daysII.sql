38cb9f7c0471148e006b28b3239658cc978aa48e
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-09 00:00:45 -0500

    Create UserActivityPast30daysII.sql

---
+++ UserActivityPast30daysII.sql
@@ -0,0 +1,3 @@
+select ifnull(round(count(distinct session_id)/count(distinct user_id),2),0.00) as average_sessions_per_user
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
