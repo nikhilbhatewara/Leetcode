44a664669060f2f526710dd8f5751bf57089fa6b
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-11 23:58:07 -0500

    Update MarketAnalysisII.sql

--- MarketAnalysisII.sql
+++ MarketAnalysisII.sql
@@ -59,3 +59,19 @@ from itemCTE
 group by user_id
 order by user_id;
 
+
+-- better solution
+
+
+select user_id as seller_id, 
+        (case 
+            when favorite_brand = (
+                            select i.item_brand
+                            from Orders o left join Items i
+                            on o.item_id = i.item_id
+                            where o.seller_id = u.user_id 
+                            order by o.order_date
+                            limit 1 offset 1
+                                  ) then "yes" else "no" end
+         ) as "2nd_item_fav_brand"   
+from Users u   
