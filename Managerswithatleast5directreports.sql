4e6c1badebda06f633932df8842a146d3b46c727
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-15 23:52:06 -0500

    Create Managerswithatleast5directreports.sql

---
+++ Managerswithatleast5directreports.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+select name
+from employee 
+where id in 
+(
+
+        select managerid
+        from employee
+        where managerid is not null
+        group by managerid
+        having count(*) >= 5
+)    
+;
