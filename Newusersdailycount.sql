b93080dec1d84ac244faeb8f353e831f690b2205
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-10-18 17:45:59 -0500

    Create Newusersdailycount.sql

---
+++ Newusersdailycount.sql
@@ -0,0 +1,33 @@
+# Write your MySQL query statement below
+
+
+with logins as
+(
+select user_id,activity_date , dense_rank() over(partition by user_id order by activity_date asc) logindays
+from traffic
+where activity = "login"
+)
+
+
+, firstlogins as
+(
+select user_id,activity_date
+from logins
+where logindays = 1 and datediff("2019-06-30",activity_date) <= 90
+)
+
+
+select activity_date as login_date, count(distinct user_id) as user_count
+from firstlogins
+group by activity_date
+having count(distinct user_id) > 0;
+
+-- smaller approach
+select login_date, count(1) user_count
+from
+(select user_id, min(activity_date) login_date
+from traffic
+where activity = 'login'
+group by user_id) a
+where login_date between date_add('2019-06-30', interval -90 day) and '2019-06-30'
+group by login_date
