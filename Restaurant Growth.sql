98fc230dc95d886b75fa71f0bad2c62fa5cddf51
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-01 18:38:29 -0600

    Create Restaurant Growth.sql

---
+++ Restaurant Growth.sql
@@ -0,0 +1,17 @@
+with ct as (
+    select 
+        visited_on,
+        sum(amount) as amount
+    from Customer
+    group by visited_on)
+
+
+select
+    c1.visited_on,
+    sum(c2.amount) as amount,
+    round(avg(c2.amount), 2) as average_amount
+from ct c1, ct c2
+where datediff(c1.visited_on, c2.visited_on)  between 0 and 6
+group by c1.visited_on
+having count(c2.visited_on) = 7
+
