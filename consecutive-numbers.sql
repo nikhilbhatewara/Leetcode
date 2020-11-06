2db2813c3dd4a38d4a454d5ccf8f04161ff3e3f3
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-05 23:26:43 -0600

    Update consecutive-numbers.sql

--- consecutive-numbers.sql
+++ consecutive-numbers.sql
@@ -9,3 +9,26 @@ inner join logs c
 on b.id + 1 = c.id and b.num = c.num
 
 
+
+
+
+with cte1 as 
+
+(
+
+SELECT l1.id,l1.num as cnum,l1.num - l2.num as diff
+
+FROM Logs l1
+join Logs l2
+
+on l1.id + 1 = l2.id
+
+)
+select distinct cnum
+from cte1
+
+where diff = 0
+
+group by cnum
+
+having count(cnum) > 1
