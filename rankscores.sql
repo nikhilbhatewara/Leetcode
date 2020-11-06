7251ab8712f91064093fb46388ed27b15b471e82
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-05 23:10:20 -0600

    Update rankscores.sql

--- rankscores.sql
+++ rankscores.sql
@@ -8,3 +8,15 @@ from scores d left join (select b.score,
 			order by b.score desc) as c
     on c.score = d.score
 order by 1 desc
+
+
+-- Use dense rank to avoid gaps
+
+select score,myrank  as `rank`
+from
+(
+
+SELECT id,score,dense_rank() over(order by score desc) as myrank
+FROM Scores
+) as t
+order by myrank 
