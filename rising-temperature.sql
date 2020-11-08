642d34903f3d1450eb766cdc59ce410bd30ba833
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-07 20:04:00 -0600

    Update rising-temperature.sql

--- rising-temperature.sql
+++ rising-temperature.sql
@@ -7,3 +7,16 @@ inner join Weather w2
 on datediff(w1.recordDate,w2.recordDate) = 1
 and w1.temperature > w2.temperature 
 
+
+
+-- window function
+
+select Id_INT_
+from
+(
+
+SELECT Id_INT_,RecordDate_DATE_,Temperature_INT_
+,lag(Temperature_INT_,1) over() as prev_temp
+FROM Weather
+  ) as T
+  where Temperature_INT_ > prev_temp
