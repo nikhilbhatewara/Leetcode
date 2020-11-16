541c8b4d3ad4be0d768a8b21192d1d031113220e
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-16 00:32:44 -0600

    Update Customers Who Bought All Products.sql

--- Customers Who Bought All Products.sql
+++ Customers Who Bought All Products.sql
@@ -9,3 +9,23 @@ having count(distinct a.product_key) = (
     select count(*) from product
 
  )
+
+-- can be done without joining
+
+/*
+count the no of distinct product in product table 
+for each custoemr count the no of distinct products they have purchased
+filter table
+*/
+
+
+
+select customer_id
+from Customer 
+group by customer_id
+having count(distinct product_key) in (
+
+SELECT count(distinct product_key) as pcount
+FROM Product
+  
+  )
