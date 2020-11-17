b82e29c08c3094abf9d9e4ca83fb0fd7739208be
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-17 08:05:21 -0600

    Update 1097 GamePlayAnalysisV.sql

--- 1097 GamePlayAnalysisV.sql
+++ 1097 GamePlayAnalysisV.sql
@@ -28,3 +28,50 @@ left join Activity B
 ON A.player_id = B.player_id
 and A.event_date + 1 = B.event_Date
 group by A.event_date
+
+-- my approach
+
+/*
+For each user find first login day (min event_date)
+For each first login day, count no of users(group by)
+For each login day, find no of users who logged in on day1(left join with activity table based of datediff = 1, then group by and count)
+Compute the ratio
+*/
+
+
+with firstloginCTE as
+(
+SELECT player_id,min(event_date) as first_login
+FROM Activity
+group by player_id
+)
+
+,totalPlayersday0 as
+(
+  
+  
+  select first_login,count(*) as day0_count
+  from firstloginCTE
+  group by first_login
+)
+
+
+,loginday1 as 
+(
+  select a.first_login,a.player_id,count(ifnull(b.player_id,0)) as day1_count
+  from firstloginCTE a
+  left join Activity b
+  on datediff(b.event_date,a.first_login) = 1 and a.player_id = b.player_id
+group by a.first_login,a.player_id
+)
+  
+select distinct a.first_login,a.day0_count as installs , round(b.day1_count/a.day0_count,2) as Day1_retention
+from  totalPlayersday0 a
+join loginday1 b
+on a.first_login = b.first_login
+
+  
+  
+  
+  
+  
