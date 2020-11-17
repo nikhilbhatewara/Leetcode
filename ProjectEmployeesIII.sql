bff1fa13cc8df70f4bc5505f302c888ec2ccd1b7
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-16 23:50:28 -0600

    Update ProjectEmployeesIII.sql

--- ProjectEmployeesIII.sql
+++ ProjectEmployeesIII.sql
@@ -11,3 +11,15 @@ on p.employee_id = e.employee_id
 select project_id,employee_id
 from expCTE 
 where exprank = 1;
+
+
+-- another approach
+
+
+select p.project_id, p.employee_id
+from Project p
+left join Employee e
+on p.employee_id = e.employee_id
+where e.experience_years = (select max(experience_years) from Employee)
+
+
