aca71e75e00596de56a4d1202cdf9d9e3f5ae0a6
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-16 23:40:27 -0600

    Update ProjectEmployeesII.sql

--- ProjectEmployeesII.sql
+++ ProjectEmployeesII.sql
@@ -10,3 +10,14 @@ from project
 group by project_id
 ) as t
 where emprank = 1;
+
+
+-- 
+
+select project_id
+from Project p
+left join Employee e
+on p.employee_id = e.employee_id
+group by project_id
+order by count(p.employee_id) desc limit 1
+;
