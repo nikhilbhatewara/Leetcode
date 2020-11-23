c1bc2fcb58e889fa24da33d960393dedd3454555
Nikhil Bhatewara <jain2nikhil@gmail.com>
2020-11-23 00:17:53 -0600

    Update Newusersdailycount.sql

--- Newusersdailycount.sql
+++ Newusersdailycount.sql
@@ -31,3 +31,28 @@ where activity = 'login'
 group by user_id) a
 where login_date between date_add('2019-06-30', interval -90 day) and '2019-06-30'
 group by login_date
+
+
+
+
+-- another approach
+/*
+Find first login date for each user
+filter for 90 days 
+take count
+*/
+
+with cte1 as
+(
+SELECT user_id,min(activity_date) as first_login
+FROM Traffic
+where activity = "login"
+group by user_id
+)
+
+
+select first_login as login_date, count(*)
+from cte1
+group by first_login
+having first_login between "2019-03-30" and "2019-06-30"
+order by first_login asc
