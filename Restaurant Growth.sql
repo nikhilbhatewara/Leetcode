9586a3027906b38ae0b0304fe65436b3a6c4c1b6
Nikhil Bhatewara <jain2nikhil@gmail.com>
2022-01-04 10:21:12 -0500

    Update Restaurant Growth.sql

--- Restaurant Growth.sql
+++ Restaurant Growth.sql
@@ -15,3 +15,32 @@ where datediff(c1.visited_on, c2.visited_on)  between 0 and 6
 group by c1.visited_on
 having count(c2.visited_on) = 7
 
+/*
+-------- 
+Alternative solution
+----------
+*/
+
+# Write your MySQL query statement below
+
+with agg_sum as
+(
+
+select visited_on,sum(amount) as amount
+    from Customer
+    group by 1
+
+)
+
+
+select visited_on,amount,average_amount
+from
+(
+select visited_on,
+sum(amount) over(order by visited_on  rows between 6 PRECEDING and current row) as amount,
+round(avg(amount) over(order by visited_on  rows between 6 PRECEDING and current row),2)  as average_amount,
+row_number() over(order by visited_on asc) as r_number
+from agg_sum
+order by 1 asc
+) a
+where r_number > 6;
